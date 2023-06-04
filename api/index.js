35fedcb049b0740350280d67d9f8e4892cc84a39
loveless_louvre <jfrsonstamaria@gmail.com>
2023-06-04 21:03:28 +0800

    test

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ const model = new OpenAI({
 });
 
 // Document Library and Chain
-const directoryLoader = new DirectoryLoader('./support', {
+const directoryLoader = new DirectoryLoader('support', {
 	'.txt': (path) => new TextLoader(path)
 	// TODO Add more file types
 });
@@ -53,7 +53,7 @@ const promptTemplate = new PromptTemplate({
 const llmChain = new LLMChain({ llm: model, prompt: promptTemplate });
 
 // Main API Endpoint
-app.post('/aid-bot', cors(), async (req, res) => {
+app.post('aid-bot/', cors(), async (req, res) => {
 	/*
 	 * API Process Flow
 	 * 1. Problem Processing (Sentiment Analysis, Problem Prioritization)
