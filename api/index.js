948c07fb117628f2c6f63adc9587ba9c7597fdcc
loveless_louvre <jfrsonstamaria@gmail.com>
2023-06-04 19:44:29 +0800

    nope

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ const model = new OpenAI({
 });
 
 // Document Library and Chain
-const directoryLoader = new DirectoryLoader('support/', {
+const directoryLoader = new DirectoryLoader('support', {
 	'.txt': (path) => new TextLoader(path)
 	// TODO Add more file types
 });
