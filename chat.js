3a05a41cfb4352278f02aad4f909f66a62eeca3b
loveless_louvre <jfrsonstamaria@gmail.com>
2023-06-04 17:57:32 +0800

    test port

--- chat.js
+++ chat.js
@@ -23,9 +23,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.listen(process.env.API_PORT, () => {
-	console.log(`API Port: http://localhost:${process.env.API_PORT}`);
-});
+app.listen();
 
 const model = new OpenAI({
 	openAIApiKey: process.env.OPENAI_API_KEY,
