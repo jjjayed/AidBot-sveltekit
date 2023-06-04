fc214c784b83b2e7c05e4bd1d3a4536959043f83
loveless_louvre <jfrsonstamaria@gmail.com>
2023-06-04 18:04:03 +0800

    test port again

--- src/routes/Chat/+page.svelte
+++ src/routes/Chat/+page.svelte
@@ -196,7 +196,7 @@
 		const API_ENDPOINT =
 			process.env.NODE_ENV === 'production' ? '/aid-bot' : 'http://localhost:5000/aid-bot';
 
-		fetch(API_ENDPOINT, requestOptions)
+		fetch('/aid-bot', requestOptions)
 			.then((response) => {
 				if (response.ok) {
 					return response.json();
